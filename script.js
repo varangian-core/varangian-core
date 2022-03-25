fb474001b59cc951c27648fae702239c735ddd81
varangian-core <victor.vladimir.mikhaltsevich@gmail.com>
2022-03-25 16:08:26 -0700

    Added Bootstrap Modal

--- script.js
+++ script.js
@@ -50,3 +50,22 @@ function reveal(){
     }
   }
 }
+//modal form
+
+$(document).ready(function(){
+  /* Get iframe src attribute value i.e. YouTube video url
+  and store it in a variable */
+  var url = $("#cartoonVideo").attr('src');
+
+  /* Assign empty url value to the iframe src attribute when
+  modal hide, which stop the video playing */
+  $("#myModal").on('hide.bs.modal', function(){
+    $("#cartoonVideo").attr('src', '');
+  });
+
+  /* Assign the initially stored url back to the iframe src
+  attribute when modal is displayed again */
+  $("#myModal").on('show.bs.modal', function(){
+    $("#cartoonVideo").attr('src', url);
+  });
+});
